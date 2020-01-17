df5c403eb584b0ef716fb847c4020fca80a508ac
Вершков Егор Николаевич <en.vershkov@voenet.local>
2020-01-17 08:15:24 +0400

    change doc -> docx

--- resources/views/modal/info.blade.php
+++ resources/views/modal/info.blade.php
@@ -11,10 +11,10 @@
         </div>
       <form action="{{ route($route) }}" method="post" autocomplete="off">
           @csrf
-          
+
           <div class="modal-body">
-            <input type="hidden" name="{{ $id }}" value="0"> 
-            <input type="hidden" name="ip" value="0" id="hidden_ip_buy">    
+            <input type="hidden" name="{{ $id }}" value="0">
+            <input type="hidden" name="ip" value="0" id="hidden_ip_buy">
                   <div class="form-group">
                       <input type="text" name="username" class="form-control" placeholder="Введите ваше ФИО" autofocus required>
                   </div>
@@ -25,15 +25,13 @@
                     <input type="email" name="email" class="form-control" placeholder="Ваш СЛУЖЕБНЫЙ электронный почтовый адрес " required>
                 </div>
                     <p class="alert alert-danger">Указывайте обязательно свой служебный электронный адрес (тот что заканчивается на ...@voel.ru или ...@...voel.ru), на этот адрес будет отправлена ссылка для подтверждения ставки</p>
-                    <p class="alert alert-info">После бронирования техники – <a href="<?php echo asset("storage/contract.doc")?>">заполните договор (воспользовавшись шаблоном)</a> 
-                        и направьте подписанный в двух экземплярах документ в Отдел информационных технологий вашего филиала 
-                        <br>
-                        <br> 
-                        <b>Если в течении 5-ти дней, после окончания аукциона, договор не будет предоставлен – бронь АННУЛИРУЕТСЯ.</b> 
+                    <p class="alert alert-info">После бронирования техники – <a href="<?php echo asset("storage/contract.docx")?>">заполните договор (воспользовавшись шаблоном)</a>
+                        и направьте подписанный в двух экземплярах документ в Отдел информационных технологий вашего филиала
+
                     </p>
-                    <a href="<?php echo asset("storage/contract.doc")?>" class="btn btn-primary btn-block">Скачать бланк договора</a>
+                    <a href="<?php echo asset("storage/contract.docx")?>" class="btn btn-primary btn-block">Скачать бланк договора</a>
           </div>
-  
+
               <div class="modal-footer">
                   <button type="button" class="btn btn-secondary" data-dismiss="modal">Отмена</button>
                   <button type="submit" class="btn btn-primary" >Забронировать</button>
@@ -57,8 +55,8 @@
           @csrf
           <div class="modal-body">
 
-            <input type="hidden" name="{{ $id }}" value="0">       
-            <input type="hidden" name="ip" value="0" id="hidden_ip_rebuy">       
+            <input type="hidden" name="{{ $id }}" value="0">
+            <input type="hidden" name="ip" value="0" id="hidden_ip_rebuy">
                   <div class="form-group">
                       <input type="text" name="username" class="form-control" placeholder="Введите ваше ФИО" autofocus required>
                   </div>
@@ -70,13 +68,11 @@
                   </div>
                   <p class="alert alert-danger">Указывайте обязательно свой служебный электронный адрес (тот что заканчивается на ...@voel.ru или ...@...voel.ru), на этот адрес будет отправлена ссылка для подтверждения ставки</p>
                   <p class="alert alert-warning" role="alert"><b>ВНИМАНИЕ:</b> Перекупка техники добавит +500₽ к текущей стоимости!</p>
-                    <p class="alert alert-info">После бронирования техники – <a href="<?php echo asset("storage/contract.doc")?>">заполните договор (воспользовавшись шаблоном)</a> 
-                        и направьте подписанный в двух экземплярах документ в Отдел информационных технологий вашего филиала 
-                        <br>
-                        <br> 
-                        <b>Если в течении 5-ти дней, после окончания аукциона, договор не будет предоставлен – бронь АННУЛИРУЕТСЯ.</b> 
+                    <p class="alert alert-info">После бронирования техники – <a href="<?php echo asset("storage/contract.docx")?>">заполните договор (воспользовавшись шаблоном)</a>
+                        и направьте подписанный в двух экземплярах документ в Отдел информационных технологий вашего филиала
+
                     </p>
-                    <a href="<?php echo asset("storage/contract.doc")?>" class="btn btn-primary btn-block">Скачать бланк договора</a>
+                    <a href="<?php echo asset("storage/contract.docx")?>" class="btn btn-primary btn-block">Скачать бланк договора</a>
               </div>
               <div class="modal-footer">
                   {{-- <small id=list class="text-muted">-</small> --}}
